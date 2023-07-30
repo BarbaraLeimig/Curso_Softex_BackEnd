dc991857a5f92b4642c2b7bcac756941b6d90181
Bárbara Leimig <101154290+BarbaraLeimig@users.noreply.github.com>
2023-07-30 11:35:10 -0300

    Update calculadoraIdade.js
    Refeito aplicando as boas práticas da programação

--- Logica_de_Programacao/Modulo2_Introducao_a_Programacao/atividade15/calculadoraIdade.js
+++ Logica_de_Programacao/Modulo2_Introducao_a_Programacao/atividade15/calculadoraIdade.js
@@ -1,35 +1,44 @@
+
 const readline = require('readline-sync');
 
-let validYear = false;
-
-while (validYear == false) {
-    try {
-
-        let name = readline.question("Digite o seu nome completo: ");
-        let year = readline.questionInt("Digite o ano em que voce nasceu: ");
-
-        if (isNaN(year) == false) {
-
-            if (year >= 1922 && year <= 2021) {
-
-                validYear = true;
-                let age = 2022 - year;
-
-                console.log(`\nNome completo: ${name}`);
-                console.log(`Voce completou ou completará ${age} anos em 2022.`);
-
-            } else {
-
-                validYear = false;
-                throw new Error("\nO ano informado não se encontra entre 1922 e 2021");
-
-            }
-        } else {
-
-            throw new Error("\nO Valor digitado não é um numero. Digite novamente!");
-
-        }
-    } catch (error) {
-        console.error(error.message);
-    }
+// Função para calcular a idade do usuário com base em seu ano de nascimento
+function calculadoraIdade(nomeCompleto, anoNascimento) {
+    const anoAtual = 2022;
+    const idade = anoAtual - anoNascimento;
+    return `Olá, ${nomeCompleto}! Em 2022 você completou ou completará ${idade} anos.`;
 }
+
+// Função para obter os dados do usuário
+function data() {
+    let nomeCompleto, anoNascimento;
+
+    // Laço de repetição para aplicar as condições solicitadas na atividade. Ler aquivo README.md
+    while (true) {
+        try {
+            // Solicita o nome completo e ano de nascimento ao usuário usando o readline-sync
+            nomeCompleto = readline.question("Digite seu nome completo: ");
+            anoNascimento = readline.questionInt("Digite o ano em que voce nasceu (valido entre 1922 e 2021: ");
+
+            // Verifica se o que foi digitado é um número inteiro e se está dentro do intervalo válido
+            if (!Number.isInteger(anoNascimento) || anoNascimento < 1922 || anoNascimento > 2021) {
+                // Caso o ano de nascimento seja inválido, lança um erro
+                throw new Error("Ano de nascimento inválido. Por favor, digite um valor numérico entre 1922 e 2021.");
+            } else {
+                // Se os valores inseridos forem válidos, sair do loop while
+                break;
+            }
+
+        } catch (error) {
+            // Captura o erro, exibe a mensagem de erro e continua o loop para solicitar novamente os dados corretos
+            console.error(error.message);
+        }
+    }
+
+    // Calcula a idade com base no ano de nascimento fornecido e exibe o resultado
+    const resultado = calculadoraIdade(nomeCompleto, anoNascimento);
+    console.log(resultado);
+
+}
+
+// Inicia a interação com o usuário para obter os dados
+data();
