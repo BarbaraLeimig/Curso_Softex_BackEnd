a88eca3af65856cea5ba508ba2c2026cf590482b
Bárbara Leimig <101154290+BarbaraLeimig@users.noreply.github.com>
2023-08-08 21:14:16 -0300

    Create matrizBidimensional.js

---
+++ Logica_de_Programacao/Modulo_3_Algortimo_e_Estrutura_de_Dados/atividade18/matrizBidimensional.js
@@ -0,0 +1,20 @@
+// Cria uma matriz bidimensional com informações sobre animais
+const animais = [
+    ["Leão", "Felino", 10],
+    ["Gato", "Mamífero", 10],
+    ["Tigre", "Felino", 8],
+    ["Girafa", 'Mamífero', 15],
+    ["Águia", "Ave", 5]
+];
+
+// Exibe informações sobre os animais
+function exibirAnimais() {
+    console.log("Nome\t\tEspécie\t\tIdade");
+    console.log("-------------------------------------");
+    for (let i = 0; i < animais.length; i++) {
+        const [nome, especie, idade] = animais[i];
+        console.log(`${nome}\t\t${especie}\t\t${idade}`);
+    }
+}
+
+exibirAnimais();
