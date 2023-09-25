23b62e8cdf80ac7811b107179a301057865bdb75
Bárbara Leimig <barbaraleimigdev@gmail.com>
2023-09-25 19:03:34 +0000

    cadastro

---
+++ JavaScript/Modulo01_Manipulacao_de_dados_em_JavaScript/atividade2/cadastro_tipos_dados.js
@@ -0,0 +1,10 @@
+let nome = "Ana Botafogo"; //Exemplo de varíavel do tipo string
+let salario = 10.524; //Exemplo de variável do tipo float
+let idade = 60; //Exemplo de variavl do tipo inteiro
+let temDiploma = true; //Exemplo de variável do tipo boolean
+
+//Imprimindo os dados
+console.log(`Nome: ${nome}`);
+console.log(`Salário: ${salario}`);
+console.log(`Idade: ${idade}`);
+console.log(`Possui Diploma? ${temDiploma ? "Sim" : "Não"}`);
